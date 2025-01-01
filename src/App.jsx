fe7e88e502d2a84503b89c1d1bebdaccf078847d
Mansi Charmkar <mansic632@gmail.com>
2025-01-01 23:05:47 +0530

    Form styles completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import FormHandling from "./components/FormHandling";
+import MultipleFormHandling from "./components/MultipleFormHandling";
 
 function App() {
   return (
     <div>
       <FormHandling />
+      <MultipleFormHandling />
     </div>
   );
 }
---
+++ src/components/MultipleFormHandling.jsx
@@ -0,0 +1,45 @@
+function MultipleFormHandling() {
+  return (
+    <div>
+      <form>
+        <div>
+          <div>
+            Name : <input type="text" />
+          </div>
+          <div>
+            Age : <input type="number" />
+          </div>
+          <div>
+            Email : <input type="email" />
+          </div>
+          <div>
+            Gender : <input type="radio" />
+          </div>
+          <div>
+            Date : <input type="date" />
+          </div>
+          <div>
+            What do you wnat to lean : <input type="checkbox" />
+          </div>
+          <div>
+            phone : <input type="tel" />
+          </div>
+          <div>
+            Parent Contact : <input type="tel" />
+          </div>
+          <div>
+            Address : <input type="text" />
+          </div>
+          <div>
+            City : <input type="text" />
+          </div>
+          <div>
+            Status : <input type="text" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default MultipleFormHandling;
